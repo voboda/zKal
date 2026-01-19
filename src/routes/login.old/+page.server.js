f061b7f6a69fd3d1c720c81ea9f6889dba4d2493
Your Name <you@example.com>
2026-01-19 17:17:24 +0200

    cleanup

--- src/routes/login.old/+page.server.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const actions = {
-  default: async () => {
-    // This action is no longer needed since we're not using cookies
-    // Just redirect to home
-    throw redirect(303, '/');
-  },
-};
